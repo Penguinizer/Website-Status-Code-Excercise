703daf271020e5194202fbd3a9273ceeaa97ab60
Penguinizer <17841108+Penguinizer@users.noreply.github.com>
2022-07-14 11:25:58 +0300

    More work
    Updated readme
    Finished xml parsing

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Purpose of the program and excercise:
 ----
  The requirements are that the program periodically makes HTTP requests to pages read from a file. The poage contents are then verified by matching a content requirement read from the file. The time required for the webserver to complete the request is also checked. This information is then written to a log file.
  
- This is accomplished through the use of a two step process. The first step is to parse the xml file using a utility function invoked by the main function. This is done both to record the polling frequency in a class variable as well as to populate a list of sites which are to be polled. The list of sites is appended onto a list (specifically a hashmap) with one half being the site, the other the string requirement.
+ This is accomplished through the use of a two step process. The first step is to parse the xml file using a utility function invoked by the main function. This is done both to record the polling frequency in a class variable as well as to populate a list of sites which are to be polled. The list of sites is appended onto a list of entries with one half being the site, the other the string requirement.
  
  Once the list of sites has been populate the second step is to utilize a secondary thread which iterates through the list, checks the site for the requirements and then logs the result. This is done utilizing the Java TimerTask and Timer classes. The class which iterates through the list implements the TimerTask abstract class which is scheduled using the Timer. This requires that the polling frequency is infrequent enough so that it is longer than the duration required to iterate through the list of sites.
  
@@ -31,5 +31,8 @@ Step by step:
 ----
  * Run program.
  * When prompted enter 1 or 2 to use the default configuration file or a custom one.
- * 
+ * If using a custom configuration file name enter the name of the file with ".xml" included. Please see xml formatting above.
+ * When prompted enter the name of the log file.
+ * Allow program to run as desired.
+ * enter "quit" to gracefully exit when desired.
   
--- src/siteStatusExcercise/WebStatusMain.java
+++ src/siteStatusExcercise/WebStatusMain.java
@@ -14,8 +14,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.Timer;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -29,22 +32,27 @@ import org.xml.sax.SAXException;
 
 public class WebStatusMain {
 	protected int pollingRate; //Note: in seconds
-	protected Map<String, String> siteMap = new HashMap<String,String>();
+	protected List<Entry<String, String>> siteMap = new ArrayList<Entry<String,String>>();
 	
 	//The main function which calls the xml parser and polling timertask.
 	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-		System.out.println("Calling xml file parsing function\n");
-		
 		//Initialize a class object so that the parseXML function can simply access those without
-		// requiring the use of returning a pair containing the polling period and site/requirement map.
-		//Also calling the parseXML function in order to read the file.
+		// requiring the use of returning a pair containing the polling period and site/requirement list.
 		WebStatusMain myMain = new WebStatusMain();
+		//Calling the parseXML function in order to read the file.
+		System.out.println("Calling xml file parsing function.");
 		myMain.parseXML();
+		
+		//After populating the list creating the Timer and TimerTasks.
+		System.out.print("Creating Timer and Timed Task to poll sites from file.\n"
+				+ "Please enter name of log file: ");
+		String logName = readInput();
+		
 	}
 	
 	//Utility functions
 	//The parseXML function parses the XML configuration file.
-	//This required some referencing of outside resources as I haven't parsed XML in a while.
+	//This required some referencing of Java docs as well as a write-up on the DOM parser as I haven't worked with it before.
 	protected void parseXML() throws ParserConfigurationException, SAXException, IOException {
 		//Allow the user to either use the default file or to enter the name of their own XML file.
 		int tmpint = 0;
@@ -79,12 +87,39 @@ public class WebStatusMain {
 		xmldoc.getDocumentElement().normalize();
 		Element xmlRoot = xmldoc.getDocumentElement();
 		
-		//Fetch the polling rate from the file.
+		//Parse the polling rate from the configuration file.
 		pollingRate = Integer.parseInt(xmlRoot.getElementsByTagName("pollingrate").item(0).getTextContent());
+		//System.out.println("Polling rate: " + pollingRate);
 		
-		//Iterate through the list of nodes and record the site addresses and requirements into a hashmap.
+		//Get the list of site nodes from the root, create temporary variables for iterating.
 		NodeList nodes = xmlRoot.getElementsByTagName("site");
-		System.out.print(nodes.getLength());
+		NodeList tmpChildren;
+		Node tmpNode;
+		String tmpAddress = "test";
+		String tmpReq = "test";
+		
+		//Use nested loops to iterate through the list of sites and to iterate through the child
+		// entries of those site entries in order to get the site address and requirements.
+		for(int itint = 0; itint < nodes.getLength(); itint++) {
+			//Get the site entry from the NodeList.
+			tmpNode = nodes.item(itint);
+			//Get the site entry's child nodes.
+			tmpChildren = tmpNode.getChildNodes();
+			//Iterate through entry's children.
+			for(int secit = 0; secit < tmpChildren.getLength(); secit++) {
+				tmpNode = tmpChildren.item(secit);
+				//Check that the children contain the address and requirements.
+				//Get the text contents if they do and save in temp variables.
+				if (tmpNode.getNodeName().equals("address")) {
+					tmpAddress = tmpNode.getTextContent();
+				} 
+				else if (tmpNode.getNodeName().equals("requirement")) {
+					tmpReq = tmpNode.getTextContent();
+				}
+			}
+			//Use Put to append the address and requirement to the list of entries.
+			siteMap.add(new SimpleEntry<String, String>(tmpAddress, tmpReq));
+		}
 	}
 	
 	//A function for reading the user's input.
